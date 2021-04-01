8c9b60abb321b7fba686a85eb63060b3ce6d95a7
Nic McDonald <n.mcdonald83@gmail.com>
2021-04-01 10:51:09 -0600

    Add sweep.py, fix benchmark.json, update README

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # desbench
 
 A benchmark application for libdes
+
+# Build System
+libdes/desbench uses the [Bazel](https://bazel.build/) build system. Bazel is [easy to install](https://docs.bazel.build/versions/master/install.html).
+
+# Dependencies
+The only system level dependency libdes/desbench requires is libnuma:
+``` sh
+sudo apt install libnuma-dev
+```
+Everything else is automatically compiled in by bazel.
+
+# Build desbench
+``` sh
+bazel build -c opt --copt=-O3 --copt=-g --copt=-flto --linkopt=-flto ...
+```
+
+# Run desbench
+Run the benchmark simulator with default settings:
+``` sh
+./bazel-bin/desbench cfg/benchmark.json
+```
+
+Run the benchmark simulator with full parallelism:
+``` sh
+./bazel-bin/desbench cfg/benchmark.json /simulator/core/executers=int=$(nproc)
+```
+
+Run a full benchmark suite (requires some python3 packages):
+
+This will take 13*3*nproc*5 seconds (26 minutes for 8 thread machine)
+``` sh
+./scripts/sweep.py ./bazel-bin/desbench output -r 3 -e 5 -s 1
+```
---
+++ scripts/sweep.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy
+import os
+import pprint
+import re
+import taskrun
+
+def main():
+  ap = argparse.ArgumentParser()
+  ap.add_argument('exe', help='desbench program')
+  ap.add_argument('odir', help='the output directory')
+  ap.add_argument('-r', '--runs', type=int, default=1,
+                  help='number of runs')
+  ap.add_argument('-e', '--exetime', type=int, default=10,
+                  help='execution time per run in seconds')
+  ap.add_argument('-s', '--step', type=int, default=4,
+                  help='step size for number of cpus (threads)')
+  ap.add_argument('-n', '--numactl', default=None,
+                  help="arguments for numactl");
+  ap.add_argument('-v', '--verbose', action='store_true',
+                  help="show task descriptions");
+  args = ap.parse_args();
+
+  if not os.path.isdir(args.odir):
+    os.mkdir(args.odir)
+
+  rm = taskrun.ResourceManager(
+    taskrun.CounterResource('cpus', os.cpu_count(), os.cpu_count()))
+  cob = taskrun.FileCleanupObserver()
+  vob = taskrun.VerboseObserver(show_description=args.verbose)
+  tm = taskrun.TaskManager(resource_manager=rm,
+                           observers=[cob, vob],
+                           failure_mode=taskrun.FailureMode.AGGRESSIVE_FAIL)
+
+  cpus_start = 1
+  cpus_stop = os.cpu_count()
+  cpus_step = args.step
+  cpus_list = [x for x in range(cpus_start, cpus_stop + 1)
+               if x % cpus_step == 0]
+  runs = args.runs
+
+  # Makes the default settings
+  cfg_file = os.path.join(args.odir, 'settings.json')
+  with open(cfg_file, 'w') as fd:
+    cfg = {
+      'simulator': {
+        'execution_time': 'TBD',
+        'core': {
+          'executers': 'TBD',
+          'seed': 1234,
+          'observer_interval': 1.0,
+          'observer_power': 11
+        },
+        'mapper': {
+          'algorithm': 'round_robin'
+        },
+        'observer': {
+          'log_summary': True
+        },
+        'logger': {
+          'file': '-'
+        }
+      },
+      'benchmark': {
+        'num_components': 1000,
+        'component': {
+          'type': 'TBD',
+          'initial_events': '$&(/benchmark/num_components)&$',
+          'look_ahead': 1,
+          'stagger_tick': False,
+          'stagger_epsilon': False,
+          'remote_probability': 0.0
+        }
+      },
+      'debug': []
+    }
+    json.dump(cfg, fd, indent=2)
+
+  # Generates the models
+  models = {
+    'empty-self': [
+      '/benchmark/component/type=string=empty'],
+    'empty-all': [
+      '/benchmark/component/type=string=empty',
+      '/benchmark/component/remote_probability=float=1'],
+    'simple-self': [
+      '/benchmark/component/type=string=simple'],
+    'simple-all': [
+      '/benchmark/component/type=string=simple',
+      '/benchmark/component/remote_probability=float=1'],
+    'mem1m-1b': [
+      '/benchmark/component/type=string=memory',
+      '/benchmark/component/bytes=uint=1000',
+      '/benchmark/component/size=uint=1'],
+    'mem10m-1b': [
+      '/benchmark/component/type=string=memory',
+      '/benchmark/component/bytes=uint=10000',
+      '/benchmark/component/size=uint=1'],
+    'mem100m-1b': [
+      '/benchmark/component/type=string=memory',
+      '/benchmark/component/bytes=uint=100000',
+      '/benchmark/component/size=uint=1'],
+    'mem1g-1b': [
+      '/benchmark/component/type=string=memory',
+      '/benchmark/component/bytes=uint=1000000',
+      '/benchmark/component/size=uint=1'],
+    'mem10g-1b': [
+      '/benchmark/component/type=string=memory',
+      '/benchmark/component/bytes=uint=10000000',
+      '/benchmark/component/size=uint=1'],
+    'bounce1': [
+      '/benchmark/component/type=string=empty',
+      '/benchmark/component/initial_events=uint=1',
+      '/benchmark/component/remote_probability=float=1'],
+    'bounce2': [
+      '/benchmark/component/type=string=empty',
+      '/benchmark/component/initial_events=uint=2',
+      '/benchmark/component/remote_probability=float=1'],
+    'bounce5': [
+      '/benchmark/component/type=string=empty',
+      '/benchmark/component/initial_events=uint=5',
+      '/benchmark/component/remote_probability=float=1'],
+    'bounce10': [
+      '/benchmark/component/type=string=empty',
+      '/benchmark/component/initial_events=uint=10',
+      '/benchmark/component/remote_probability=float=1'],
+  }
+
+  for model in sorted(models):
+    for cpus in cpus_list:
+      for run in range(0, runs):
+        name = '{0}_{1}_{2}'.format(model, cpus, run)
+        filename = os.path.join(args.odir, name + '.log')
+        cmd = ''
+        if args.numactl:
+          cmd += 'numactl {} '.format(args.numactl)
+        cmd += args.exe + ' '
+        cmd += cfg_file + ' '
+        cmd += '/simulator/execution_time=float={} '.format(args.exetime)
+        cmd += '/simulator/core/executers=uint={} '.format(cpus)
+        for mod in models[model]:
+          cmd += mod + ' '
+        task = taskrun.ProcessTask(tm, name, cmd)
+        task.stdout_file = filename
+        task.add_condition(taskrun.FileModificationCondition(
+          [], [filename]))
+
+  tm.randomize()
+  res = tm.run_tasks()
+  if not res:
+    return -1
+
+  # gather all data
+  data = {}
+  for model in sorted(models):
+    data[model] = {'rate': []}
+    for cpus in cpus_list:
+      runtime_sum = 0
+      rate_sum = 0
+      for run in range(0, runs):
+        name = '{0}_{1}_{2}'.format(model, cpus, run)
+        filename = os.path.join(args.odir, name + '.log')
+        rate = extractRate(filename)
+        rate_sum += rate
+      rate_sum /= runs
+      data[model]['rate'].append(rate_sum)
+
+  # print the CSV file
+  with open(os.path.join(args.odir, 'throughput.csv'), 'w') as fd:
+    # print the header
+    print('Benchmark,', file=fd, end='')
+    for cpus in cpus_list:
+      print('{},'.format(cpus), file=fd, end='')
+    print('', file=fd)
+
+    # print each row
+    for model in sorted(models):
+      print('{},'.format(model), file=fd, end='')
+      for idx in range(len(cpus_list)):
+        print('{},'.format(data[model]['rate'][idx]), file=fd, end='')
+      print('', file=fd)
+
+  # plot the PNG file
+  cmap = plt.get_cmap('gist_rainbow')
+  colors = [cmap(idx) for idx in numpy.linspace(0, 1, len(models))]
+
+  fig = plt.figure(figsize=(16,10))
+  ax1 = fig.add_subplot(1, 1, 1)
+  ax1.set_xlabel('Number of threads')
+  ax1.set_ylabel('Events per second')
+  ax1.grid(True)
+  ax1.set_xlim(cpus_list[0], cpus_list[-1])
+  lines = []
+  maxy = 0
+  for idx, model in enumerate(sorted(models)):
+    line, = ax1.plot(cpus_list, data[model]['rate'], color=colors[idx],
+                     lw=1, label=model)
+    lines.append(line)
+    maxy = max(maxy, max(data[model]['rate']))
+  ax1.set_ylim(bottom=0, top=maxy*1.1)
+  labels = [line.get_label() for line in lines]
+  ax1.legend(lines, labels, loc='upper left', fancybox=True, shadow=True,
+             ncol=1)
+  fig.tight_layout()
+  fig.savefig(os.path.join(args.odir, 'throughput.png'))
+
+
+def extractRate(filename):
+  with open(filename, 'r') as fd:
+    for line in fd:
+      if line.find('Events per second') == 0:
+        words = line.split()
+        return float(words[-1])
+  assert False, 'Never found the keywords in {}'.format(filename)
+
+
+if __name__ == '__main__':
+  main()
